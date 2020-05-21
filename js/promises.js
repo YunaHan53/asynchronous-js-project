7d3705297331df6b8c91b2d9f5174301373ac058
YunaH <YunaH503@gmail.com>
2020-05-20 23:11:45 -0400

    Added .finally() to add Loading message to HTML

--- js/promises.js
+++ js/promises.js
@@ -41,9 +41,14 @@ function generateHTML(data) {
 }
 
 btn.addEventListener('click', (event) => {
+  event.target.textContent = "Loading..."
+
   getJSON(astrosUrl)
     .then(getProfiles)
     .then(generateHTML)
-    .catch(err => console.log(err))
-  event.target.remove();
+    .catch(err => {
+      peopleList.innerHTML = '<h3>Something Went Wrong!</h3>'
+      console.log(err)
+    })
+    .finally( () =>   event.target.remove() )
 });
